f6fd5d8f89ccdf281fb4fd272a1cd5ed07dd727d
mari7102 <55129539+mari7102@users.noreply.github.com>
2020-09-10 19:56:15 +0200

    Add files via upload

---
+++ Hjemmeside med java/app.js
@@ -0,0 +1,29 @@
+﻿const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+   burger.addEventListener('click', () => { 
+
+ //Toggle Nav
+    burger.addEventListener('click', () =>{
+       nav.classList.toggle('nav-active');
+    });
+
+
+
+    //Animat Links
+      navLinks.forEach((link, index) => {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+
+        });
+
+          //Burger Animation
+          burger.classList.toggle('toogle');
+     });
+}
+
+
+navSlide();
+
---
+++ Hjemmeside med java/validering.js
@@ -0,0 +1,52 @@
+﻿window.addEventListener("DOMCententLoaded", function () {
+    var errorCount;
+
+    var myForm = document.forms["contactForm"];
+
+    var fullname = myForm["fullname"];
+    var email = myForm["email"];
+    var phonenumber = myForm["phonenumber"];
+    var message = myForm["message"];
+
+    var lblFullname = fullname.nextElementSibling;
+    var lblEmail = email.nextElementSibling;
+    var lblPhonenumber = phonenumber.nextElementSibling;
+    var lblMessage = message.nextElementSibling;
+
+    var regexEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+
+    myForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        errorCount = 0;
+        lblFullname.innerHTML = "";
+        lblEmail.innerHTML = "";
+        lblPhonenumber.innerHTML = "";
+
+
+        if (fullname.value.lenght == 0) {
+            lblFullname.innerHTML = "Full Name"
+            errorCount++;
+        }
+
+        if (!regexEmail.test(email.value)) {
+            lblEmail.innerHTML = "Not correct";
+            errorCount++;
+        }
+
+            if (phonenumber.value.lenght == 0) {
+                lblPhonenumber = "Phonenumber"
+                errorCount++;
+            }
+
+            if (message.value.lenght == 0) {
+                lblPhonenumber = "Message"
+                errorCount++;
+            }
+
+            if (errorCount == 0) {
+                myForm.submit();
+            }
+
+            
+        });
+});
